a748449f4d224ae99b8132e2ab3d59cba0f05c66
Jhoncito el pillo <jhon.electronica@gmail.com>
2020-07-14 17:12:07 -0500

    CRUD with Postman

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesAPI = require('./routes/movies.js');
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
-})
-
-app.get('/json', function(req, res) {
-  res.json({ hello: 'world' });
-});
+moviesAPI(app);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
--- routes/movies.js
+++ routes/movies.js
@@ -3,15 +3,54 @@ const { moviesMock } = require('../utils/mocks/movies');
 
 function moviesAPI(app) {
   const router = express.Router();
-  app.user("/api/movies", router);
+  app.use("/api/movies", router);
 
-  router.get('/', async function(req, res, next) {
+  router.get('/:moviesId', async function(req, res, next) {
     try {
-        const movies = await Promise.resolve(moviesMock)
+        const movies = await Promise.resolve(moviesMock[0])
 
         res.status(200).json({
           data: movies,
-          message: 'movies listed'
+          message: 'movie retrieved'
+        })
+    } catch(err) {
+      next(err)
+    }
+  })
+
+  router.post('/', async function(req, res, next) {
+    try {
+        const createdMovieId = await Promise.resolve(moviesMock[0].id)
+
+        res.status(201).json({
+          data: createdMovieId,
+          message: 'movie created'
+        })
+    } catch(err) {
+      next(err)
+    }
+  })
+
+  router.put('/:movieId', async function(req, res, next) {
+    try {
+        const updatedMovieId = await Promise.resolve(moviesMock[0].id)
+
+        res.status(200).json({
+          data: updatedMovieId,
+          message: 'movie updated'
+        })
+    } catch(err) {
+      next(err)
+    }
+  })
+
+  router.delete('/:movieId', async function(req, res, next) {
+    try {
+        const deletedMovieId = await Promise.resolve(moviesMock[0].id)
+
+        res.status(200).json({
+          data: deletedMovieId,
+          message: 'movies deleted'
         })
     } catch(err) {
       next(err)
