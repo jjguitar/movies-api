ed267be2040906694003ff1a9bde8f1314364430
Jhoncito el pillo <jhon.electronica@gmail.com>
2020-07-21 16:19:34 -0500

    Create userMoviesAp

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
-const moviesAPI = require('./routes/movies');
+const moviesApi = require('./routes/movies.js');
+const userMoviesApi = require('./routes/userMovies.js');
 
-const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandler')
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers.js');
 
-const notFoundHandler = require('./utils/middleware/notFoundHandler')
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
+// body parser
 app.use(express.json());
 
-moviesAPI(app);
+// routes
+moviesApi(app);
+userMoviesApi(app);
+
 // Catch 404
 app.use(notFoundHandler);
 
-// //Error middleware
+// Errors middleware
 app.use(logErrors);
 app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
-  console.log(`Listening http://localhost:${config.port}`); // eslint-disable-line
+  console.log(`Listening http://localhost:${config.port}`);
 });
